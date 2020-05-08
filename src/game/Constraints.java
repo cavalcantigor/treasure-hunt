f5275b532817191f3a3aba00001f9a5ed94a56c0
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-05-08 20:32:18 -0300

    adding rules package

---
+++ src/game/Constraints.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Constraints {
+	public static int MIN_PLAYERS = 2;
+	public static int MAX_PLAYERS = 4;
+	public static int DIG_POINTS_MULTIPLIER = 5;
+}
--- src/main/THunt.java
+++ src/main/THunt.java
@@ -2,26 +2,17 @@
 package main;
 
 import java.util.Scanner;
-import player.Player;
-import util.Constraints;
+import game.Player;
 
 
 public class THunt {
 
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
-		Player[] players;
-		int nPlayers;
+		int nPlayers = 0;
 		
 		System.out.println("How many players out there? ");
 		nPlayers = scan.nextInt();
-		if (!Constraints.validateMinimumPlayers(nPlayers)) {
-			System.out.println("Sorry... invalid number of players.");
-			System.out.println("It should be between " + Constraints.MIN_PLAYERS + " and "
-					+ Constraints.MAX_PLAYERS + ".");
-			scan.close();
-			return;
-		}
 		
 		scan.close();
 	}
