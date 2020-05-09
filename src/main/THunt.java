782f476948e308db4882792aac2c120e9902cdfd
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-05-08 21:57:00 -0300

    adding validate nplayers and validate player name

--- src/main/THunt.java
+++ src/main/THunt.java
@@ -3,6 +3,7 @@ package main;
 
 import java.util.Scanner;
 import game.Player;
+import rules.PlayerRule;
 
 
 public class THunt {
@@ -10,9 +11,14 @@ public class THunt {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 		int nPlayers = 0;
+		Player[] players;
 		
-		System.out.println("How many players out there? ");
-		nPlayers = scan.nextInt();
+		nPlayers = PlayerRule.validateNPlayers(scan);
+		players = new Player[nPlayers];
+		for(int i = 0; i < nPlayers; i++) {
+			players[i] = new Player();
+			PlayerRule.validatePlayerName(scan, players[i], i + 1);
+		}
 		
 		scan.close();
 	}
--- src/rules/PlayerRule.java
+++ src/rules/PlayerRule.java
@@ -1,5 +1,46 @@
 package rules;
 
-public class PlayerRule {
+import java.util.Scanner;
 
+import game.Constraints;
+import game.Player;
+
+public class PlayerRule {
+	
+	public static int validateNPlayers(Scanner scan) {
+		int n = 0;
+		while(true) {
+			System.out.println("How many players out there? ");
+			n = scan.nextInt();
+			if (n < Constraints.MIN_PLAYERS || n > Constraints.MAX_PLAYERS) {
+				System.out.println("Invalid data! Number of players should be between " + 
+						Constraints.MIN_PLAYERS + " and " + Constraints.MAX_PLAYERS);
+				System.out.println("Please, re-enter data...");
+			} else {
+				break;
+			}
+		}
+		return n;
+	}
+	
+	public static void validatePlayerName(Scanner scan, Player player, int i) {
+		String name; // player name from standard input
+		
+		// infinite loop
+		while(true) {
+			// reading name property from i'th player
+			System.out.println("[Player " + i + "] Whats your name? ");
+			name = scan.nextLine(); // reading name and surname
+			
+			// split by regex "blank space" and generate an array of Strings
+			if (name.split("\\s+").length == 1) {
+				System.out.println("Invalid data! Should enter name and surname");
+				System.out.println("Please, re-enter data...");
+			} else {
+				// when length name is more than 1, then breaks while loop
+				break;
+			}
+		}
+		player.setName(name);
+	}
 }
