5d51e371bc686600983433edd5d7dbeee28b5384
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-05-09 16:26:04 -0300

    adding new rules

--- src/rules/PlayerRule.java
+++ src/rules/PlayerRule.java
@@ -1,5 +1,6 @@
 package rules;
 
+import java.util.Random;
 import java.util.Scanner;
 
 import game.Constraints;
@@ -11,11 +12,11 @@ public class PlayerRule {
 		int n = 0;
 		while(true) {
 			System.out.println("How many players out there? ");
-			n = scan.nextInt();
+			n = Integer.valueOf(scan.nextLine());
 			if (n < Constraints.MIN_PLAYERS || n > Constraints.MAX_PLAYERS) {
 				System.out.println("Invalid data! Number of players should be between " + 
 						Constraints.MIN_PLAYERS + " and " + Constraints.MAX_PLAYERS);
-				System.out.println("Please, re-enter data...");
+				System.out.println("Please, re-enter players...");
 			} else {
 				break;
 			}
@@ -24,7 +25,7 @@ public class PlayerRule {
 	}
 	
 	public static void validatePlayerName(Scanner scan, Player player, int i) {
-		String name; // player name from standard input
+		String name;
 		
 		// infinite loop
 		while(true) {
@@ -35,12 +36,47 @@ public class PlayerRule {
 			// split by regex "blank space" and generate an array of Strings
 			if (name.split("\\s+").length == 1) {
 				System.out.println("Invalid data! Should enter name and surname");
-				System.out.println("Please, re-enter data...");
+				System.out.println("Please, re-enter name and surname...");
 			} else {
 				// when length name is more than 1, then breaks while loop
 				break;
 			}
 		}
 		player.setName(name);
+	}
+	
+	public static void validatePlayerAge(Scanner scan, Player player, int i) {
+		int age;
+		
+		// infinite loop
+		while(true) {
+			// reading age property from i'th player
+			System.out.println(player.getName() + ", what's your age? ");
+			age = Integer.valueOf(scan.nextLine()); // reading player age
+			
+			if (age < Constraints.MIN_PLAYER_AGE) {
+				System.out.println("Invalid data! Age should be greater or equal " + Constraints.MIN_PLAYER_AGE);
+				System.out.println("Please, re-enter age...");
+			} else {
+				break;
+			}
+		}
+		player.setAge(age);
+	}
+	
+	public static void validatePlayerDigPoints(Scanner scan, Player player) {
+		// instantiate object random to generate random numbers
+		Random r = new Random();
+		
+		// generate random numbers between max (inclusive) and min (inclusive)
+		int digPoints = r.nextInt(Constraints.MAX_PLAYER_DIG_POINTS + 1) + Constraints.MIN_PLAYER_DIG_POINTS;
+		
+		// set dig point to player
+		player.setDigPoints(digPoints);
+		
+		// set pirate points
+		player.setPiratePoints(
+				Constraints.PIRATE_POINTS - (Constraints.DIG_POINTS_MULTIPLIER * player.getDigPoints()) 
+		);
 	}
 }
