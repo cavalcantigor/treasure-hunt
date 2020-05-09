35bece876c343b0ec11494273af1fa2e2c3c89b3
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-05-09 16:56:41 -0300

    adding dig points constraints

--- src/game/Constraints.java
+++ src/game/Constraints.java
@@ -3,5 +3,9 @@ package game;
 public class Constraints {
 	public static int MIN_PLAYERS = 2;
 	public static int MAX_PLAYERS = 4;
+	public static int MIN_PLAYER_AGE = 12;
 	public static int DIG_POINTS_MULTIPLIER = 5;
+	public static int PIRATE_POINTS = 100;
+	public static int MIN_PLAYER_DIG_POINTS = 4;
+	public static int MAX_PLAYER_DIG_POINTS = 7;
 }
